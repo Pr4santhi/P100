5f4314e6baae989d28acbebb5494ee902b2d0512
Pr4santhi <73959648+Pr4santhi@users.noreply.github.com>
2022-01-28 16:59:02 +0000

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,8 @@
+class Atm(object):
+    def __init__(atmCardNumber,pinNumber):
+        self.atmCardNumber=atmCardNumber
+        self.pinNumber=pinNumber
+     
+
+    def display(self):
+        print("displayed")
